763245e2df4ba40e9f5d390f85fb49943f6a4809
Jeff Brown <jeffbrowndev@gmail.com>
2022-11-12 01:10:26 -0800

    refactors

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -5,33 +5,28 @@ import { Player } from "./Player";
 
 export class Game implements IGame
 {
-    private _player: Player;
-    private _actionMananger: IActionManager;
-    private _sceneRenderer: ISceneRenderer;
+    private player: Player;
+    private actionMananger: IActionManager;
+    private sceneRenderer: ISceneRenderer;
 
     public constructor(player: Player, actionManager: IActionManager, sceneRenderer: ISceneRenderer)
     {
-        this._sceneRenderer = sceneRenderer;
-        this._player = player;
-        this._actionMananger = actionManager;
+        this.sceneRenderer = sceneRenderer;
+        this.player = player;
+        this.actionMananger = actionManager;
         
         document.onclick = (e) => this.update(e.target);
     }
 
-    private get player(): Player
-    {
-        return this._player;
-    }
-
     private update(element: EventTarget | null): void
     {
-        this._actionMananger.handleClick(element);
+        this.actionMananger.handleClick(element);
 
         this.render();
     }
 
     public render(): void
     {
-        this._sceneRenderer.update(this.player.playerState);
+        this.sceneRenderer.update(this.player.playerState);
     }
 }
